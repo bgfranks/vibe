2e8bc0ae9439f3122a5d95b2df20e9a3ff0d988b
bgfranks <bgfranks.dev@gmail.com>
2025-07-28 15:43:19 -0400

    set up inngest and ran a mock background job

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { TRPCReactProvider } from '@/trpc/client';
 
+import { Toaster } from 'sonner';
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -30,6 +32,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
+          <Toaster />
         </body>
       </html>
     </TRPCReactProvider>
