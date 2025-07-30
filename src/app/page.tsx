22be0f439911016a427ceeab3075ffe0a642773e
bgfranks <bgfranks.dev@gmail.com>
2025-07-29 20:25:42 -0400

    set up openai with inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@
 
 import { useTRPC } from '@/trpc/client';
 import { useMutation } from '@tanstack/react-query';
-import { toast } from 'sonner';
+import { useState } from 'react';
 
+import { toast } from 'sonner';
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 export default function Home() {
+  const [value, setValue] = useState('');
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -17,12 +20,16 @@ export default function Home() {
   );
   return (
     <div className='p-4 max-w-7xl mx-auto'>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: 'Test' })}
+        onClick={() => invoke.mutate({ value: value })}
       >
         Invoke Background Job
       </Button>
+      <button className='bg-blue-500 bg-opacity-30 backdrop-blur-md hover:bg-opacity-50 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition duration-300 ease-in-out'>
+        Log In
+      </button>
     </div>
   );
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,24 @@
+import { Agent, openai, createAgent } from '@inngest/agent-kit';
+
 import { inngest } from './client';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
   async ({ event, step }) => {
-    await step.sleep('wait-a-moment', '1s');
-    return { message: `Hello ${event.data.email}!` };
+    const codeAgent = createAgent({
+      name: 'code-agent',
+      system:
+        'You are an expert next.js developer. You write readable, maintainable, and clean code. You write simple next.js and React sippets',
+      model: openai({ model: 'gpt-4o' }),
+    });
+
+    const { output } = await codeAgent.run(
+      `write the following snippet: ${event.data.value}`
+    );
+
+    console.log(output);
+
+    return { output };
   }
 );
