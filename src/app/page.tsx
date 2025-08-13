31e0cae0551a11a9e5efe109de197b2755863813
bgfranks <bgfranks.dev@gmail.com>
2025-08-12 21:00:59 -0400

    updated primsa to have the messages schema. shange the trpc routes to save the messages and fragements to the db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useTRPC } from '@/trpc/client';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
 import { toast } from 'sonner';
@@ -11,10 +11,15 @@ import { Input } from '@/components/ui/input';
 export default function Home() {
   const [value, setValue] = useState('');
   const trpc = useTRPC();
-  const invoke = useMutation(
-    trpc.invoke.mutationOptions({
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
       onSuccess: () => {
-        toast.success('Background job invoked successfully!');
+        toast.success('Meassage created!');
+      },
+      onError: (e) => {
+        toast.error(`Error: ${e.message}`);
       },
     })
   );
@@ -22,11 +27,12 @@ export default function Home() {
     <div className='p-4 max-w-7xl mx-auto'>
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
-        disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ value: value })}
+        disabled={createMessage.isPending}
+        onClick={() => createMessage.mutate({ value: value })}
       >
         Invoke Background Job
       </Button>
+      {JSON.stringify(messages, null, 2)}
     </div>
   );
 }
